855e91069d5a06211fb0f5a31ed5bd139b5b618a
JA <joshappeldev@gmail.com>
2019-04-05 10:12:49 -0400

    radios work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,35 +5,36 @@ let questionBank = [];
 let timer;
 
 class Question {
-  constructor(question, answers, correctAnswer) {
-    this.question = question
-    this.answers = answers
-    this.correctAnswer = correctAnswer
+  constructor(question, choices, correctAnswer) {
+    this.question = question;
+    this.choices = choices;
+    this.correctAnswer = correctAnswer;
   }
 }
 
 const question1 = new Question(
   "Which group released the hit song, 'Smells Like Teen Spirit'?",
   ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
-  "Nirvana")
+  "Nirvana"
+);
 
 const question2 = new Question(
   "What was Doug's best friend's name?",
   ["Skeeter", "Mark", "Zach", "Cody"],
   "Skeeter"
-)
+);
 
 const question3 = new Question(
   "What was the name of the principal at Bayside High in Saved By The Bell?",
   ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
   "Mr.Belding"
-)
+);
 
-Question.prototype.addQuestion = function (arr, question) {
-  arr.push(question)
-}
+Question.prototype.addQuestion = function(arr, question) {
+  arr.push(question);
+};
 
-questionBank.push(question1, question2, question3)
+questionBank.push(question1, question2, question3);
 
 class Quiz {
   constructor(correct, incorrect, counter) {
@@ -43,9 +44,9 @@ class Quiz {
   }
 }
 
-let thisQuiz = new Quiz(0, 0, 10);
+let thisQuiz = new Quiz(0, 0, 120);
 
-Quiz.prototype.countdown = function () {
+Quiz.prototype.countdown = function() {
   this.counter--;
   console.log(this.counter);
   $("#counter-number").html(this.counter);
@@ -55,7 +56,7 @@ Quiz.prototype.countdown = function () {
   }
 };
 
-Quiz.prototype.start = function () {
+Quiz.prototype.start = function(arr) {
   timer = setInterval(this.countdown.bind(this), 1000);
 
   $("#sub-wrapper").prepend(
@@ -66,31 +67,26 @@ Quiz.prototype.start = function () {
 
   $("#start").remove();
 
-  for (questions of questionBank) {
-    const {
-      question,
-      answers
-    } = questions
+  arr.forEach((quizQuestion, index) => {
+    const { question, choices } = quizQuestion;
     card.append(`<h2>${question}</h2>`);
-    console.log(answers)
-    for (answer of answers) {
+    console.log(choices);
+    for (choice of choices) {
       card.append(
         `<div class="form-check">
-        <input class="form-check-input" type="radio" id="exampleRadios1" value="${
-          answer
-        }">
-        <label class="form-check-label" for="exampleRadios1">
-        ${answer}
+        <input class="form-check-input" name="${index}" type="radio" id="${choice}" value="${choice}">
+        <label class="form-check-label" for="${choice}">
+        ${choice}
         </label>
       </div>`
       );
     }
-  }
+  });
 
   card.append(`<button class="btn btn-danger" id='done'>Done</button>`);
 };
 
-Quiz.prototype.done = function () {
+Quiz.prototype.done = function() {
   let inputs = $(".form-check").children(".form-check-input:checked");
   for (let i = 0; i < inputs.length; i++) {
     if ($(inputs[i]).val() === questionBank[i].correctAnswer) {
@@ -102,23 +98,23 @@ Quiz.prototype.done = function () {
   this.result();
 };
 
-Quiz.prototype.result = function () {
+Quiz.prototype.result = function() {
   clearInterval(timer);
 
   $("#sub-wrapper h2").remove();
 
   card.html(`<h2>All Done!</h2>`);
-  card.append(`<h3>Correct Answers: ${this.correct}</h3>`);
-  card.append(`<h3>Incorrect Answers: ${this.incorrect}</h3>`);
+  card.append(`<h3>Correct choices: ${this.correct}</h3>`);
+  card.append(`<h3>Incorrect choices: ${this.incorrect}</h3>`);
   card.append(`<button class="btn btn-success" id="start">Start</button>`);
 };
 
-$(document).on("click", "#start", function () {
-  thisQuiz = new Quiz(0, 0, 10);
-  thisQuiz.start();
+$(document).on("click", "#start", function() {
+  thisQuiz = new Quiz(0, 0, 120);
+  thisQuiz.start(questionBank);
 });
 
-$(document).on("click", "#done", function () {
+$(document).on("click", "#done", function() {
   thisQuiz.done();
 });
 
