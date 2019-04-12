3b9226c1fc2cc9f14895ec2271db2a3f67adfbb1
JA <joshappeldev@gmail.com>
2019-04-11 23:34:30 -0400

    remove most global variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,13 +4,15 @@
 let timer;
 //declare variable for the quiz object
 let thisQuiz;
-let repeatedQuizIndex = []
-let quizzesAlreadyTaken = []
-
-//keep track of quiz totals since a new quiz object if being created for each new quiz.
-let totalCorrect = 0
-let totalIncorrect = 0
-let totalQuestionCount = 0
+// keep track of quiz totals since a new quiz object is being created for each new quiz.
+const quizzes = {
+  repeatedQuizIndex: [],
+  quizzesAlreadyTaken: [],
+  totalCorrect: 0,
+  totalIncorrect: 0,
+  totalQuestionCount: 0,
+  totalScore: 0
+};
 
 //class for making the question objects, takes in the question, choices, and correctAnswer.
 class Question {
@@ -18,8 +20,8 @@ class Question {
     this.question = question;
     this.choices = choices;
     this.correctAnswer = correctAnswer;
-    this.userAnswer = ""
-    this.title = title
+    this.title = title;
+    this.userAnswer = "";
   }
 }
 
@@ -81,80 +83,86 @@ const sciTrivia1 = new Question(
   ["Oberon", "Ganymede", "Titan", "Europa"],
   "Ganymede",
   "Science Quiz"
-)
+);
 
 const sciTrivia2 = new Question(
   "What does the 'c' in E=mc^2 stand for?",
   ["Energy", "Speed of Light", "Mass", "Dark Matter"],
   "Speed of Light",
   "Science Quiz"
-)
+);
 
 const sciTrivia3 = new Question(
   "What precious stone is the hardest?",
   ["Diamond", "Ruby", "Sapphire", "Emerald"],
   "Diamond",
   "Science Quiz"
-)
+);
 
 //declare some question group arrays to use in the addQuestions method
-const oopQuiz = [oop1, oop2, oop3]
-const triviaQuiz = [trivia1, trivia2, trivia3]
-const sciQuiz = [sciTrivia1, sciTrivia2, sciTrivia3]
+const oopQuiz = [oop1, oop2, oop3];
+const triviaQuiz = [trivia1, trivia2, trivia3];
+const sciQuiz = [sciTrivia1, sciTrivia2, sciTrivia3];
 //flatten this array but can't use .flat() bc edge is poop.
-const comboQuiz = [oopQuiz, sciQuiz, triviaQuiz].reduce((a, b) => a.concat(b), [])
+const comboQuiz = [oopQuiz, sciQuiz, triviaQuiz].reduce(
+  (a, b) => a.concat(b),
+  []
+);
 
-//class blueprint for new Quiz objects, sets correct and incorrect to 0, sets questionsArray and quizQuestionBanks to an empty array 
+//class blueprint for new Quiz objects, sets correct and incorrect to 0, sets questionsArray and quizQuestionBanks to an empty array
 class Quiz {
   constructor() {
     this.correct = 0;
     this.incorrect = 0;
     this.counter = 0;
     //questionsArray will hold the quiz array for the specific instance of the quiz being generated
-    this.questionsArray = []
+    this.questionsArray = [];
     //this array holds all the questionBanks as an array of arrays, allowing the quiz to be randomly generated.
-    this.quizQuestionBanks = []
+    this.quizQuestionBanks = [];
   }
 }
 
 //method to add questions to any array. Since the exact number of questions for each quiz can be anything the rest parameter is used.
-Quiz.prototype.addQuestionBank = function (...questions) {
+Quiz.prototype.addQuestionBank = function(...questions) {
   //push our array of questionBanks
   this.quizQuestionBanks.push(...questions);
-}
+};
 //use lodash to check for 2 equal arrays
-Quiz.prototype.areEqual = function (arr1, arr2) {
-  return _.isEqual(arr1, arr2)
-}
+Quiz.prototype.areEqual = function(arr1, arr2) {
+  return _.isEqual(arr1, arr2);
+};
 
-Quiz.prototype.setQuestionBank = function () {
+Quiz.prototype.setQuestionBank = function() {
   //grab random index based on question bank array
-  let randomIndex = Math.floor(Math.random() * this.quizQuestionBanks.length)
-  console.log(randomIndex)
+  let randomIndex = Math.floor(Math.random() * this.quizQuestionBanks.length);
+  console.log(randomIndex);
   //add randomIndex to repeatedQuizIndex[0]
   //if repeated Quiz index does not include the random index
-  if (!repeatedQuizIndex.includes(randomIndex)) {
+  if (!quizzes.repeatedQuizIndex.includes(randomIndex)) {
     //set index to [0]
-    repeatedQuizIndex.unshift(randomIndex)
+    quizzes.repeatedQuizIndex.unshift(randomIndex);
     //set questions array to the randomly chosen bank
-    this.questionsArray = this.quizQuestionBanks[randomIndex]
+    this.questionsArray = this.quizQuestionBanks[randomIndex];
     //set counter to a certain amount of seconds per question
-    this.counter = this.questionsArray.length * 10
+    this.counter = this.questionsArray.length * 10;
     //start the quiz
-    this.startQuiz()
+    this.startQuiz();
   } else {
     //run this again
-    this.setQuestionBank()
+    this.setQuestionBank();
   }
-}
+};
 
 //method to convert seconds into minutes for the counter
-Quiz.prototype.convertTime = function (timeInSeconds) {
-  return `${Math.floor(timeInSeconds / 60)}:${(timeInSeconds % 60 < 10 ? "0" : "") + timeInSeconds % 60}`;
-}
+Quiz.prototype.convertTime = function(timeInSeconds) {
+  return `${Math.floor(timeInSeconds / 60)}:${(timeInSeconds % 60 < 10
+    ? "0"
+    : "") +
+    (timeInSeconds % 60)}`;
+};
 
 //method to randomize both the questions and answers
-Quiz.prototype.randomize = function (arr) {
+Quiz.prototype.randomize = function(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -163,7 +171,7 @@ Quiz.prototype.randomize = function (arr) {
 };
 
 //method to run the counter for the timer. Once the counter hits 0 the finishQuiz method is called.
-Quiz.prototype.runCounter = function () {
+Quiz.prototype.runCounter = function() {
   this.counter--;
   $("#counter-number").html(this.convertTime(this.counter));
   if (this.counter === 0) {
@@ -172,45 +180,45 @@ Quiz.prototype.runCounter = function () {
 };
 
 //this allows combo quizzes to combine titles
-Quiz.prototype.setTitle = function () {
-  let titleArray = []
+Quiz.prototype.setTitle = function() {
+  let titleArray = [];
   this.questionsArray.map(question => {
-    const {
-      title
-    } = question
-    titleArray.push(title)
-  })
+    const { title } = question;
+    titleArray.push(title);
+  });
   //call lodash to remove dupes
   // const $titleArray = _.uniq(titleArray)
   //use a set to remove dupes instead
-  const $titleArray = Array.from(new Set(titleArray))
+  const $titleArray = Array.from(new Set(titleArray));
   $("#quiz-wrapper").prepend(
-    `<h2 class="title my-4">${$titleArray.join(` & `)}</h2>`)
-}
+    `<h2 class="title my-4">${$titleArray.join(` & `)}</h2>`
+  );
+};
 
 //method to start the quiz takes in an array.
-Quiz.prototype.startQuiz = function () {
-  this.setTitle()
-  $(".card-bg").addClass("border-light")
+Quiz.prototype.startQuiz = function() {
+  this.setTitle();
+  $(".jumbo-style").hide();
+  $("#quiz-wrapper").addClass("mt-5");
+  $(".card-bg").addClass("border-light");
   //checking if the 2 arrays are equal before allowing quiz to be taken.
-  if (!this.areEqual(quizzesAlreadyTaken.sort(), this.quizQuestionBanks.sort())) {
+  const { quizzesAlreadyTaken } = quizzes;
+  if (
+    !this.areEqual(quizzesAlreadyTaken.sort(), this.quizQuestionBanks.sort())
+  ) {
     //setInterval method called to run the counter method every second. Bind this so it doesn't lose context.
     timer = setInterval(this.runCounter.bind(this), 1000);
 
     $(".title").after(
-      `<h2 class="my-4">Time Remaining: <span id="counter-number">${
-      this.convertTime(this.counter)
-    }</span></h2>`
+      `<h2 class="my-4">Time Remaining: <span id="counter-number">${this.convertTime(
+        this.counter
+      )}</span></h2>`
     );
 
     $("#start").remove();
     this.randomize(this.questionsArray);
     this.questionsArray.forEach((quizQuestion, index) => {
-
-      const {
-        question,
-        choices
-      } = quizQuestion;
+      const { question, choices } = quizQuestion;
 
       $("#quiz").append(`
       <h2 class="rounded my-4">${question}</h2>
@@ -231,19 +239,17 @@ Quiz.prototype.startQuiz = function () {
     <button class="mt-4 btn btn-outline-danger" id="finish">Finish</button>
   </div`);
   } else {
-    this.result()
+    this.result();
   }
 };
 
 //method to finish quiz. Checks answers accordingly and runs the result method.
-Quiz.prototype.finishQuiz = function () {
+Quiz.prototype.finishQuiz = function() {
+  const { quizzesAlreadyTaken } = quizzes;
   //push quiz here once it is finished
-  quizzesAlreadyTaken.push(this.questionsArray)
+  quizzesAlreadyTaken.push(this.questionsArray);
   for (let i = 0; i < this.questionsArray.length; i++) {
-    const {
-      correctAnswer,
-      userAnswer
-    } = this.questionsArray[i];
+    const { correctAnswer, userAnswer } = this.questionsArray[i];
     if (userAnswer === correctAnswer) {
       this.correct++;
     } else {
@@ -254,16 +260,21 @@ Quiz.prototype.finishQuiz = function () {
 };
 
 //method to show the results and clear the timer.
-Quiz.prototype.result = function () {
+Quiz.prototype.result = function() {
   clearInterval(timer);
 
   $("#quiz-wrapper h2").remove();
 
-  let score = `${((this.correct / this.questionsArray.length) * 100).toFixed(2)}%`;
-  totalCorrect += this.correct
-  totalIncorrect += this.incorrect
-  totalQuestionCount += this.questionsArray.length
-  totalScore = `${((totalCorrect / totalQuestionCount) * 100).toFixed(2)}%`
+  let score = `${((this.correct / this.questionsArray.length) * 100).toFixed(
+    2
+  )}%`;
+  quizzes.totalCorrect += this.correct;
+  quizzes.totalIncorrect += this.incorrect;
+  quizzes.totalQuestionCount += this.questionsArray.length;
+  quizzes.totalScore = `${(
+    (quizzes.totalCorrect / quizzes.totalQuestionCount) *
+    100
+  ).toFixed(2)}%`;
 
   $("#quiz").html(`
   <h2 class="finished">Finished</h2>
@@ -274,21 +285,23 @@ Quiz.prototype.result = function () {
   <h3>Incorrect: ${this.incorrect}</h3>
   <h2>Your Score: ${score}</h2>
   <hr class="my-4">
-  <h3>Total Correct: ${totalCorrect}</h3>
-  <h3>Total Incorrect: ${totalIncorrect}</h3>
-  <h2>Total Score: ${totalScore}</h3>
+  <h3>Total Correct: ${quizzes.totalCorrect}</h3>
+  <h3>Total Incorrect: ${quizzes.totalIncorrect}</h3>
+  <h2>Total Score: ${quizzes.totalScore}</h3>
   <button class="btn btn-outline-success mt-4" id="start">Start Next Quiz</button>
   `);
 
-  if (quizzesAlreadyTaken.length === this.quizQuestionBanks.length) {
+  if (quizzes.quizzesAlreadyTaken.length === this.quizQuestionBanks.length) {
     //remove start button so you can't click it again bc the browser will crash. condition in set question bank causes it.
-    $("#start").remove()
-    $(".finished").html(`<h3>You've taken all the quizzes!</h3>`)
-    $(".game-stats").append(`<button class="btn btn-outline-success mt-4" id="start-over">Start Over</button>`)
+    $("#start").remove();
+    $(".finished").html(`<h3>You've taken all the quizzes!</h3>`);
+    $(".game-stats").append(
+      `<button class="btn btn-outline-success mt-4" id="start-over">Start Over</button>`
+    );
   }
 };
 
-$("#quiz").on("change", ".form-check-input", function () {
+$("#quiz").on("change", ".form-check-input", function() {
   // GET question index out of "name" attribute so we know what question you answered
   const questionIndex = $(this).attr("name");
   // get value out of radio button selected
@@ -297,22 +310,22 @@ $("#quiz").on("change", ".form-check-input", function () {
   thisQuiz.questionsArray[questionIndex].userAnswer = answer;
 });
 
-$(document).on("click", "#start", function () {
+$(document).on("click", "#start", function() {
   $("#quiz").empty();
   //create newQuiz object
   thisQuiz = new Quiz();
   //add quiz question arrays declared earlier
-  thisQuiz.addQuestionBank(comboQuiz, sciQuiz, oopQuiz, triviaQuiz)
+  thisQuiz.addQuestionBank(comboQuiz, sciQuiz, oopQuiz, triviaQuiz);
   //set the questionBank to the new quiz
-  thisQuiz.setQuestionBank()
+  thisQuiz.setQuestionBank();
   //start quiz
   // thisQuiz.startQuiz();
 });
 
-$(document).on("click", "#finish", function () {
+$(document).on("click", "#finish", function() {
   thisQuiz.finishQuiz();
 });
 
-$(document).on("click", "#start-over", function () {
-  window.location.reload()
-})
+$(document).on("click", "#start-over", function() {
+  window.location.reload();
+});
